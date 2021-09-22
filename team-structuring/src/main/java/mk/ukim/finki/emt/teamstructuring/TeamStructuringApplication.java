2a4d0acf86728a6173a21fa360dc558a075a58fe
FilipSpirovski <spirovskii.filipp@gmail.com>
2021-09-22 18:15:05 +0200

    initial commit

---
+++ team-structuring/src/main/java/mk/ukim/finki/emt/teamstructuring/TeamStructuringApplication.java
@@ -0,0 +1,16 @@
+package mk.ukim.finki.emt.teamstructuring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.validation.Validator;
+
+@SpringBootApplication
+public class TeamStructuringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamStructuringApplication.class, args);
+    }
+
+}
